b7fbc1ca661607bb2dc85a8761b2817ac3685b82
ipeerless <ipeerless0@gmail.com>
2022-09-23 01:22:49 +0200

    flutter simple work

---
+++ flutter1/lib/next_page/next_page.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flutter/material.dart';
+
+class NextPage extends StatelessWidget {
+  const NextPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(),
+    );
+    
+  }
+}
