4018c90b8469d7b89225dc9468d322df8a160fc6
ipeerless <ipeerless0@gmail.com>
2022-09-30 10:47:49 +0200

    new work

--- flutter5/test/widget_test.dart
+++ flutter5/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flutter_app5/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const WebViewApp());
+    await tester.pumpWidget(const SignUpApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
